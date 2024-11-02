aa5dceeccba50d14ce49d102b3d0dee1d394455b
limedulth <julliosantana900@gmail.com>
2024-11-01 21:27:58 -0300

    ATT

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menuBtn");
     const overlay = document.getElementById ("overlay");
     const subCad = document.getElementById ("btnsubCad");
     const subCaixa = document.getElementById ("subCaixa");
+    
 
     menuBtn.onclick = function () {
         menuSidebar.classList.toggle("active");
@@ -22,26 +23,19 @@ const menuBtn = document.getElementById("menuBtn");
 
 
     }
-    btnDiario.onclick = function () {
-        relAnual.classList.toggle ("hidden");
-        relSemestre.classList.toggle ("hidden");
-        relTrimestre.classList.toggle ("hidden");
-        relMensal.classList.toggle ("hidden");
-        relSemanal.classList.toggle ("hidden");
-        relDiario.classList.toggle ("showRel");
-
-
-
+    notifyIcon.onclick = function () {
+        notifyModal.classList.toggle("showModalnotify");
+        overlayModal.classList.toggle("show");
+        notifyNumber.style.display = 'none'
     }
-    btnSemanal.onclick = function () {
-        relAnual.classList.toggle ("hidden");
-        relSemestre.classList.toggle ("hidden");
-        relTrimestre.classList.toggle ("hidden");
-        relMensal.classList.toggle ("hidden");
-        relDiario.classList.toggle ("hidden");
-        relSemanal.classList.toggle ("showRel");
-
-
-
-
+    overlayModal.onclick = function () {
+        notifyModal.classList.toggle("showModalnotify");
+        overlayModal.classList.toggle("show");
     }
+    
+
+
+
+
+
+    
